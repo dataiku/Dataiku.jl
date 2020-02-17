ea984c9dd97b4b73ae23ad6a01a84314efc26ff0
Célian Haydont <celian.haydont@dataiku.com>
2020-02-17 17:13:31 +0100

    add script to generate precompile.jl fiel

---
+++ generate_precompile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# There should be a ~/.dataiku/config.json file existing with url and api key
+# of an existing dss instance for this script to work
+
+script_path=$(dirname $0)
+
+julia --color=yes --trace-compile=$script_path"/new_precompile.jl" $script_path"/test/runtests.jl"
