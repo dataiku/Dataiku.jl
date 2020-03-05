91d7a8332adb678078beb758cdf6077f530a6eb6
Célian Haydont <celian.haydont@dataiku.com>
2020-03-05 17:13:46 +0100

    update precompile.jl file

--- scripts/generate_sysimage.jl
+++ scripts/generate_sysimage.jl
@@ -3,6 +3,6 @@
 if length(ARGS) != 2
     println("usage: julia generate_sysimage.jl PRECOMPILE.jl SYSIMAGE.so")
 else
-    using PackageCompilerX
+    using PackageCompiler
     create_sysimage(:Dataiku; precompile_statements_file=ARGS[1], sysimage_path=ARGS[2])
 end
